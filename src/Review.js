dcaee67ed61a5286fe75c9095ae7179854c9ce30
JoeBuydemDips <ksarkodie111@icloud.com>
2022-02-27 18:42:44 -0500

    Build reviews component.

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,40 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  /*
+  get index from array and set it based on button clicks 
+  */
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div button="button-container">
+          <button className="prev-btn" onClick={() => {}}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={() => {}}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn" onClick={() => {}}>
+          surprise me
+        </button>
+      </article>
+    </>
+  );
 };
 
 export default Review;
